c593eb54a58d117fdf378d28ac0ad64544bd387f
gqlao0420 <64816955+gqlao0420@users.noreply.github.com>
2025-12-09 21:50:05 +0800

    Update policies.py

--- stable_baselines3/common/policies.py
+++ stable_baselines3/common/policies.py
@@ -536,7 +536,7 @@ class ActorCriticPolicy(BasePolicy):
         else:
             # Actor与Critic分别使用不同的特征提取器(网络)
             self.pi_features_extractor = self.features_extractor # 这个是将Actor使用的特征提取器指向已经定义好的特征提取器A！！！
-            self.vf_features_extractor = self.make_features_extractor() # 定义特征提取器，假定命名 - B，
+            self.vf_features_extractor = self.make_features_extractor() # 实例化特征提取器，假定命名 - B，
                 # self.make_features_extractor()继承自BaseModel，要注意每次使用self.make_features_extractor()就会创建一个新的class！！！否则，会理解成if-else中Actor-Critic的特征提取器是一样的！
 
         self.log_std_init = log_std_init

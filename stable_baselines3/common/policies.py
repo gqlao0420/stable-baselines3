a3fa217e0f9ac62bdd1bcb2f91000ceca3ecb8aa
gqlao0420 <64816955+gqlao0420@users.noreply.github.com>
2025-12-10 22:36:12 +0800

    Update policies.py

--- stable_baselines3/common/policies.py
+++ stable_baselines3/common/policies.py
@@ -556,6 +556,7 @@ class ActorCriticPolicy(BasePolicy):
         self.dist_kwargs = dist_kwargs
 
         # Action distribution
+        # make_proba_distribution() 返回的是 Distribution 基类的不同子类的实例
         self.action_dist = make_proba_distribution(action_space, use_sde=use_sde, dist_kwargs=dist_kwargs)
 
         self._build(lr_schedule)
